8ad8bb11c8d8de19a90e78b9cb912cbff1de068d
Marcelo Rodriguez Gaete <marcelo20022010@hotmail.com>
2023-07-03 18:02:37 -0300

    arreglado de la funcion

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -76,18 +76,18 @@ function SignIn() {
     }
     const renderCreateUserInfo = () => {
       return (
-        <form>
-
+        <form ref={form} className='flex flex-col gap-4 w-80'>
           <div className='flex flex-col gap-1'>
             <label htmlFor="name" className='font-light text-sm'>Your name:</label>
             <input
               type="text"
               id="name"
               name="name"
-              defaultValue={parsedAccount.name}
+              defaultValue={parsedAccount?.name}
               placeholder="Peter"
-              className='rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
-              />
+              className='rounded-lg border border-black placeholder:font-light
+              placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
+            />
           </div>
           <div className='flex flex-col gap-1'>
             <label htmlFor="email" className='font-light text-sm'>Your email:</label>
@@ -95,10 +95,11 @@ function SignIn() {
               type="text"
               id="email"
               name="email"
-              defaultValue={parsedAccount.email}
+              defaultValue={parsedAccount?.email}
               placeholder="hi@helloworld.com"
-              className='rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
-              />
+              className='rounded-lg border border-black
+              placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
+            />
           </div>
           <div className='flex flex-col gap-1'>
             <label htmlFor="password" className='font-light text-sm'>Your password:</label>
@@ -106,17 +107,20 @@ function SignIn() {
               type="text"
               id="password"
               name="password"
-              defaultValue={parsedAccount.password}
+              defaultValue={parsedAccount?.password}
               placeholder="******"
-              className='rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
-              />
+              className='rounded-lg border border-black
+              placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4'
+            />
           </div>
-          <button
-            className='bg-black text-white w-full rounded-lg py-3'
-            onClick={() => {setView('user-info'), editAccount()}}>
-            Edit
-          </button>
-              </form>
+          <Link to="/">
+            <button
+              className='bg-black text-white w-full rounded-lg py-3'
+              onClick={() => createAnAccount()}>
+              Create
+            </button>
+          </Link>
+        </form>
       )
     }
 
